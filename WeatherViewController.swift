f096237a075b7865eaf48f171ef87e66fe137743
WONGI HONG <dt95@naver.com>
2023-07-07 18:00:39 +0900

    MOD : escaping 클로저를 통해서 데이터 불러오는 방식을 수정

--- WeatherViewController.swift
+++ WeatherViewController.swift
@@ -3,18 +3,13 @@ import Alamofire
 
 class WeatherViewController: UIViewController {
     @IBOutlet weak var cityNameLabel: UILabel!
-    @IBOutlet weak var iconImage: UIImageView!
+    @IBOutlet weak var iconImageView: UIImageView!
     @IBOutlet weak var weatherLabel: UILabel!
     @IBOutlet weak var tempLabel: UILabel!
     @IBOutlet weak var minLabel: UILabel!
     @IBOutlet weak var maxLabel: UILabel!
     @IBOutlet weak var pressureLabel: UILabel!
     
-    let url: String = "https://api.openweathermap.org/data/2.5/weather?"
-    let parameters: [String] = [
-        "appid=be7239b92f1e7c5bde30444b35b3946d",
-        "&q=Seoul"
-    ]
     let weatherAPI = WeatherAPI()
     
     override func viewDidLoad() {
@@ -23,48 +18,21 @@ class WeatherViewController: UIViewController {
     }
     
     func setWeatherData() {
-        guard let weatherData = weatherAPI.weatherData else { return }
-        
-        cityNameLabel.text = weatherData.name
-        weatherLabel.text = weatherData.weather[0].main
-        tempLabel.text = String(weatherData.main.temp)
-        minLabel.text = "최저 온도 : \(String(weatherData.main.tempMin))"
-        maxLabel.text = "최고 온도 : \(String(weatherData.main.tempMax))"
-        pressureLabel.text = "기압 : \(String(weatherData.main.pressure))"
-        
-        print(weatherData.weather[0].main)
-        
-        getWeatherImage(weatherStatus: weatherData.weather[0].main)
+        weatherAPI.getWeatherData { weatherData in
+            self.weatherLabel.text = weatherData.weather[0].main
+            self.cityNameLabel.text = weatherData.name
+            self.tempLabel.text = String(weatherData.main.temp)
+            self.minLabel.text = "최저 온도 : \(String(weatherData.main.tempMin))"
+            self.maxLabel.text = "최고 온도 : \(String(weatherData.main.tempMax))"
+            self.pressureLabel.text = "기압 : \(String(weatherData.main.pressure))"
+        } image: { iconImage in
+            DispatchQueue.main.async {
+                self.iconImageView.image = iconImage
+            }
+        }
     }
     
     @IBAction func generateButton(_ sender: UIButton) {
-        weatherAPI.getWeatherData(url: url, appid: parameters[0], city: parameters[1])
-        
         setWeatherData()
-    }
-}
-
-extension WeatherViewController: WeatherImageProtocol {
-    func getWeatherImage(weatherStatus: String) {
-        var status: String = ""
-        
-        switch weatherStatus {
-        case "clear sky":
-            status = "https://openweathermap.org/img/wn/01d@2x.png"
-        case "Rain":
-            status = "https://openweathermap.org/img/wn/10d@2x.png"
-        default:
-            print("Nothing")
-        }
-        
-        DispatchQueue.global().async {
-            if let imageURL = URL(string: status) {
-                if let imageData = try? Data(contentsOf: imageURL) {
-                    DispatchQueue.main.async {
-                        self.iconImage.image = UIImage(data: imageData)
-                    }
-                }
-            }
-        }
     }
 }
