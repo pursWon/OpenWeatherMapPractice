2a6c480c13629fe4703d055f6673b59d1e0adc5b
WONGI HONG <dt95@naver.com>
2023-07-11 18:46:11 +0900

    ADD : 경도, 위도 전달하는 delegate 패턴 생성

--- SelectCityViewController .swift
+++ SelectCityViewController .swift
@@ -1,9 +1,14 @@
 import UIKit
 
+protocol SendGeoData {
+    func setGeoData(geoData: (lon: String, lat:String))
+}
+
 class SelectCityViewController: UIViewController {
     @IBOutlet weak var cityTableView: UITableView!
     
     var cityData: City?
+    var delegate: SendGeoData?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,17 +33,28 @@ extension SelectCityViewController: UITableViewDelegate, UITableViewDataSource {
         return cityData.city.count
     }
     
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 90
-    }
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = cityTableView.dequeueReusableCell(withIdentifier: "CityTableViewCell", for: indexPath) as? CityTableViewCell else { return UITableViewCell() }
-        guard let cityData = cityData else { return UITableViewCell() }
+        guard
+            let cell = cityTableView.dequeueReusableCell(withIdentifier: "CityTableViewCell", for: indexPath) as? CityTableViewCell,
+            let cityData = cityData
+        else { return UITableViewCell() }
         
         cell.cityLabel.text = cityData.city[indexPath.row].name
         
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 90
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let weatherVC = self.storyboard?.instantiateViewController(withIdentifier: "WeatherViewController") as? WeatherViewController else { return }
+        guard let cityData = cityData else { return }
+        
+        delegate?.setGeoData(geoData: (String(cityData.city[indexPath.row].lon), String(cityData.city[indexPath.row].lat)))
+        
+        self.navigationController?.popViewController(animated: true)
     }
 }
 
