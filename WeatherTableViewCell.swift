2a130aefac22756432ffe8c4ed94c79250f5c0b5
WONGI HONG <dt95@naver.com>
2023-07-11 18:42:01 +0900

    ADD : WeatherTableView Cell 생성

---
+++ WeatherTableViewCell.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class WeatherTableViewCell: UITableViewCell {
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var tempertureLabel: UILabel!
+}
