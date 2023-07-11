2506f5ebfb75c44e146c40e1fce351387bb49c1d
WONGI HONG <dt95@naver.com>
2023-07-11 18:41:38 +0900

    ADD : weatherVC에 tableView 관련 코드 추가 및 위도, 경도 데이터  수신

--- WeatherViewController.swift
+++ WeatherViewController.swift
@@ -8,16 +8,21 @@ class WeatherViewController: UIViewController {
     @IBOutlet weak var tempLabel: UILabel!
     @IBOutlet weak var minLabel: UILabel!
     @IBOutlet weak var maxLabel: UILabel!
+    @IBOutlet weak var weatherTableView: UITableView!
     
     let weatherAPI = WeatherAPI()
+    
+    var cityGeoData: (lon: String, lat: String) = ("", "")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        iconImageView.backgroundColor = UIColor.black.withAlphaComponent(0.4)
+        setIntialWeather()
     }
     
-    func setWeatherData() {
-        weatherAPI.getWeatherData { weatherData in
+    func setIntialWeather() {
+        weatherAPI.getCurrentWeatherData(setCity: "Seoul") { weatherData in
             self.weatherLabel.text = weatherData.weather[0].main
             self.cityNameLabel.text = weatherData.name
             self.tempLabel.text = String(weatherData.main.temp)
@@ -30,7 +35,32 @@ class WeatherViewController: UIViewController {
         }
     }
     
-    @IBAction func generateButton(_ sender: UIButton) {
-        setWeatherData()
+    @IBAction func cityChangeButton(_ sender: UIButton) {
+        guard let selectVC = self.storyboard?.instantiateViewController(withIdentifier: "SelectCityViewController") as? SelectCityViewController else { return }
+        
+        selectVC.delegate = self
+        self.navigationController?.pushViewController(selectVC, animated: true)
+    }
+}
+
+extension WeatherViewController: SendGeoData {
+    func setGeoData(geoData: (lon: String, lat: String)) {
+        cityGeoData = (geoData.lon, geoData.lat)
+    }
+}
+
+extension WeatherViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = self.weatherTableView.dequeueReusableCell(withIdentifier: "WeatherTableViewCell", for: indexPath) as? WeatherTableViewCell else { return UITableViewCell() }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
     }
 }
