34d3506e7ac4d3402443b55e2156c656147f4cae
WONGI HONG <dt95@naver.com>
2023-07-11 18:43:19 +0900

    ADD : 5일동안의 날씨 데이터를 받는 model 생성

---
+++ FiveDaysData .swift
@@ -0,0 +1,36 @@
+
+import Foundation
+
+// MARK: - FiveDaysData
+struct FiveDaysData: Codable {
+    let list: [List]
+}
+
+// MARK: - List
+struct List: Codable {
+    let main: TempInform
+    let weather: [WeatherInform]
+    let dtTxt: String
+    
+    enum CodingKeys: String, CodingKey {
+        case main
+        case weather
+        case dtTxt = "dt_txt"
+    }
+}
+
+// MARK: - TempInform
+struct TempInform: Codable {
+    let tempMin, tempMax: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case tempMin = "temp_min"
+        case tempMax = "temp_max"
+    }
+}
+
+// MARK: - Weather
+struct WeatherInform: Codable {
+    let description: String
+    let icon: String
+}
